6f641edb0b63eeaf9ac14954f168ec87a9de04e2
anwesh-sarkar <anwesh.sarkar@gmail.com>
2020-06-13 21:49:38 -0400

    Finished till 2.1

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 
-const { users } = require('./data/users');
+const { users } = require("./data/users");
 
 let currentUser = {};
 
@@ -12,17 +12,33 @@ const handleFourOhFour = (req, res) => {
   res.status(404).send("I couldn't find what you're looking for.");
 };
 
+// homepage
+const handleHomepage = (req, res) => {
+  res.status(200).render("pages/homepage", { users: users });
+};
+
+// profile page
+const handleProfilePage = (req, res) => {
+  let id = req.params.id;
+  console.log(id);
+  res.send(id);
+};
+
 // -----------------------------------------------------
 // server endpoints
 express()
-  .use(morgan('dev'))
-  .use(express.static('public'))
+  .use(morgan("dev"))
+  .use(express.static("public"))
   .use(express.urlencoded({ extended: false }))
-  .set('view engine', 'ejs')
+  .set("view engine", "ejs")
 
   // endpoints
+  .get("/", handleHomepage)
+
+  // profile page endpoint
+  .get("/users/:id", handleProfilePage)
 
   // a catchall endpoint that will send the 404 message.
-  .get('*', handleFourOhFour)
+  .get("*", handleFourOhFour)
 
-  .listen(8000, () => console.log('Listening on port 8000'));
+  .listen(8000, () => console.log("Listening on port 8000"));
